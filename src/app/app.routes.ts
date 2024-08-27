399d033678f18eba5b369bca007aedb7bc646150
Jisu Jeong <hitchhiker1021@gmail.com>
2024-08-27 16:48:46 +1000

    add login UI and signup component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },  // 기본 경로를 로그인 페이지로 리디렉션
+  { path: '**', redirectTo: '/login' }  // 유효하지 않은 경로 처리
+];
