5bc81fc5411f94266254a1425f7d88373257bc69
Jisu Jeong <hitchhiker1021@gmail.com>
2024-08-29 14:44:53 +1000

    new components; group_admin, channel, super_admin

---
+++ src/app/group-admin/group-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-admin.component.html',
+  styleUrl: './group-admin.component.css'
+})
+export class GroupAdminComponent {
+
+}
