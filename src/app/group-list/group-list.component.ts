d1d8e646aa296a45b9e1ac6f81bb0c422c8d57c8
Jisu Jeong <hitchhiker1021@gmail.com>
2024-08-31 10:00:00 +1000

    modifying Card CSS

--- src/app/group-list/group-list.component.ts
+++ src/app/group-list/group-list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+import { Group } from '../models/group.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-group-list',
-  standalone: true,
-  imports: [],
   templateUrl: './group-list.component.html',
-  styleUrl: './group-list.component.css'
+  styleUrls: ['./group-list.component.css'],
+  imports:[CommonModule],
+  standalone:true
 })
-export class GroupListComponent {
+export class GroupListComponent implements OnInit {
+  userGroups: Group[] = [];
 
+  ngOnInit(): void {
+    const loggedInUserEmail = sessionStorage.getItem('loggedInUserEmail');
+  
+    if (loggedInUserEmail) {
+      const storedUsers = JSON.parse(localStorage.getItem('users') || '[]');
+      const user = storedUsers.find((u: User) => u.email === loggedInUserEmail);
+  
+      if (user) {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.userGroups = user.groups;
+      }
+    }
+  }
+
+  getUserGroups(): Group[] {
+    const userJson = localStorage.getItem('currentUser');
+    const user: User = userJson ? JSON.parse(userJson) : null;
+    return user ? user.groups : [];
+  }
 }
